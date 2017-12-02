97e8cdd9c739d327a45954cde3a8f8fbb9706185
Алексей Харинов <unregistered33@gmail.com>
2017-12-02 13:53:39 +0000

    delEsp.ino отредактирован онлайн на Bitbucket

--- delEsp.ino
+++ delEsp.ino
@@ -3,7 +3,7 @@
 #include "Provider.h"
 #include <map>
 #include <functional>  
-#include <utility> 
+#include <utility> //надо проверить
 #include <vector>
 #include <list>
 #include <ESP8266WiFi.h>
